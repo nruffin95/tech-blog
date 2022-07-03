f07cacc5bd02bbe307af7c49790c57fcbc9aea58
nruffin95 <nruffin95@gmail.com>
2022-07-03 13:05:20 -0500

    Crud complete

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -28,4 +28,58 @@ const saveForm = async (req, res) => {
   await res.redirect('/')
 }
 
-module.exports = { allPost, postForm, saveForm };
+const editForm = async (req, res) => {
+  const {id} = await req.params;
+  const post = await Post.findOne({
+    where: {
+      id: id,
+    },
+    raw: true,
+  }).catch((error) => console.log(error));
+    res.render("edit", {post});
+}
+
+const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  const selector = {where:{id:id}}
+  await Post.update(data, selector).catch((error) => console.log(error));
+  res.redirect("/");
+};
+
+const viewPost = async (req, res) => {
+  const { id } = await req.params;
+  const post = await Post.findOne({
+    where: {
+      id: id,
+    },
+    raw: true,
+  }).catch((error) => console.log(error));
+  res.render("post", { post });
+};
+
+const deletePost = async (req, res) => {
+  const { id } = await req.params;
+  const post = await Post.destroy({
+    where: {
+      id: id,
+    },
+    raw: true,
+  }).catch((error) => console.log(error));
+
+  res.redirect("/");
+};
+
+
+
+
+
+module.exports = {
+  allPost,
+  postForm,
+  saveForm,
+  editForm,
+  updatePost,
+  viewPost,
+  deletePost,
+};
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,21 @@
 const router = require("express").Router();
-const { allPost, postForm, saveForm } = require("./UserController");
+const {
+  allPost,
+  postForm,
+  saveForm,
+  editForm,
+  updatePost,
+  viewPost,
+  deletePost,
+} = require("./UserController");
 
 router.get("/", allPost);
 router.get("/create", postForm);
 router.post("/create", saveForm);
+router.get("/edit/:id", editForm);
+router.post("/update/:id", updatePost);
+router.get("/post/:id", viewPost);
+router.get("/delete/:id", deletePost);
 
 // Home Route Example
 router.get('/', (req, res) => {
